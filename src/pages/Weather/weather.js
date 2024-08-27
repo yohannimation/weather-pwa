509e7164e871ea083dde27966639df50ca7ea76b
Yohann <yoyo24du26@gmail.com>
2024-08-27 20:04:32 +0200

    Changing the structure of `Weather` page

--- src/pages/Weather/weather.js
+++ src/pages/Weather/weather.js
@@ -1,16 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { checkRequiredCookies, getCurrentWeather } from './useWeather';
+
+import Loading from '../../components/Loading'
+import WeatherFooter from '../../components/WeatherFooter';
+import WeatherHeader from '../../components/WeatherHeader';
+
+import styles from './weather.module.css';
 
 function Weather() {
-    const nom = localStorage.getItem('cityName');
-    const latitude = localStorage.getItem('cityLatitude');
-    const longitude = localStorage.getItem('cityLongitude');
+    checkRequiredCookies();
+
+    const [isLoading, setIsLoading] = useState(styles.loaderOn);
+    const [mainClass, setMainClass] = useState(styles.mainWithoutData);
+    const [mainBackgroundColor, setMainBackgroundColor] = useState({backgroundColor: "var(--default-light-color)"});
+    const [currentWeather, setCurrentWeather] = useState();
+
+    // Fetch current data
+    useEffect( () => {
+        async function fetchCurrentWeather() {
+            if (!currentWeather) {
+                try {
+                    setCurrentWeather(await getCurrentWeather());
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+        fetchCurrentWeather();
+
+        // Check if the current weather data are set
+        if (
+            currentWeather !== undefined &&
+            Object.keys(currentWeather).length
+        ) {
+            // Set the background color of the app
+            if (currentWeather.weatherCode.value > 3) {
+                // Weather code for raining
+                setMainBackgroundColor({backgroundColor: "var(--rain-first-color)"});
+            } else {
+                if (currentWeather.isDay.value) {
+                    // Weather code for day and clear sky
+                    setMainBackgroundColor({backgroundColor: "var(--day-first-color)"});
+                } else {
+                    // Weather code for night and clear sky
+                    setMainBackgroundColor({backgroundColor: "var(--night-first-color)"});
+                }
+            }
+        }
+    }, [currentWeather])
+
+    setTimeout(() => {
+        setIsLoading(styles.loaderOff)
+        setMainClass(styles.mainDataSet)
+    }, 1000)
 
     return (
-        <div>
-            weather<br/>
-            {nom}<br/>
-            {latitude}<br/>
-            {longitude}<br/>
+        <div className={styles.root} style={mainBackgroundColor}>
+            <div className={isLoading}>
+                <Loading />
+            </div>
+            <div className={styles.weatherContent}>
+                <WeatherHeader currentWeather={currentWeather} />
+                <main className={mainClass}>
+                    <div className={styles.test}>
+                        <div className={styles.testHeader}>
+                            <p>~27°C</p>
+                            <p>header</p>
+                        </div>
+                        <p>content</p>
+                    </div>
+                </main>
+                <WeatherFooter />
+            </div>
         </div>
     );
 }
