97a00ed490a5243a0008106b06e6f32539f8d498
Yohann <yrenauld@myges.fr>
2024-11-10 17:50:39 +0100

    Editing Loading component

--- src/components/Loading/loading.js
+++ src/components/Loading/loading.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // CSS
 import styles from './loading.module.css'
 
 const Loading = props => {
+    const {
+        isLoading
+    } = props;
+    
+    const [rootClass, setRootClass] = useState(styles.unactive);
+
+    useEffect(() => {
+        if (isLoading) {
+            setRootClass(styles.root);
+        } else {
+            setRootClass(styles.unactive);
+        }
+    }, [isLoading])
 
     return (
-        <div className={styles.root}>
+        <div className={rootClass}>
             <div className={styles.spinner}>
 
             </div>
+            <span className={styles.background}></span>
         </div>
     );
 }
