0bf49e748973ff837b82bdecc429316953b6bc76
Yohann <yrenauld@myges.fr>
2024-12-10 13:10:02 +0100

    Creating a serviceWorker WIP

--- src/index.js
+++ src/index.js
@@ -16,3 +16,20 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using the default scope.
+
+  navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`).then(
+    registration => {
+      console.log('Service worker registration succeeded:', registration);
+    },
+    /*catch*/ error => {
+      console.error(`Service worker registration failed: ${error}`);
+    }
+  );
+} else {
+  console.error('Service workers are not supported.');
+}
