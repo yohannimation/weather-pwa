e5d41252335411ec6d8db0a8bb89abe4b357ec28
Yohann <yrenauld@myges.fr>
2025-01-18 16:58:17 +0100

    feat(modal): adding closing functionality

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 // Components
 import Button from '../Button';
+import Icon from '../Icon';
 import Title from '../Title';
 
 // CSS
@@ -13,7 +14,8 @@ const Modal = props => {
         title,
         message,
         buttonText,
-        action
+        action,
+        closeCallback = false
     } = props;
 
     const [rootClass, setRootClass] = useState(styles.unactive);
@@ -26,13 +28,14 @@ const Modal = props => {
         }
     }, [active])
 
-    // TODO when user click on the background, the modal will be close
-
     return (
         <div className={rootClass}>
             <span className={styles.background}></span>
             <div className={styles.container}>
-                <Title size={2}>{title}</Title>
+                <div className={styles.header}>
+                    <Title size={2}>{title}</Title>
+                    {closeCallback ? <span className={styles.closeButton} onClick={closeCallback}><Icon size={30} name="close" /></span> : ""}
+                </div>
                 <span className={styles.message}>{message}</span>
                 <div className={styles.buttonContainer}>
                     <Button triggerAction={action}>{buttonText}</Button>
