c6b92fc0b739b6a6b3c3bb32c3c92e64aa562926
Yohann <yoyo24du26@gmail.com>
2026-04-20 16:15:48 +0200

    fix(modal): removing the default function

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -23,7 +23,7 @@ const Modal: React.FC<ModalProps> = ({
     message,
     buttonText,
     action,
-    closeCallback = () => {}
+    closeCallback
 }) => {
     const [rootClass, setRootClass] = useState(styles.unactive);
 
